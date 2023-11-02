4f53ae6958db20f23ba8d58f5c12c4478dac9eff
Matteo <matteo.courquin@fortuneo.com>
2023-11-02 18:43:26 +0100

    :bug: Try to fix deploy bug

--- app.vue
+++ app.vue
@@ -24,6 +24,7 @@ fetchData($client).then((data) => {
 <template>
   <div>
     <h1>Test BEL</h1>
+    <p>fix bug</p>
     <p class="red-text">{{ useProjects() }}</p>
     <img style="width: 300px"
       src="https://images.unsplash.com/photo-1698869955404-601e3b410184?auto=format&fit=crop&q=80&w=2656&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
