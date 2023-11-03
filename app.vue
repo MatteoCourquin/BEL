e6b03034baed547b0f29832dd3590f30ddf1941a
Matteo <matteo.courquin@fortuneo.com>
2023-11-03 08:55:00 +0100

    :bug: Add scss

--- app.vue
+++ app.vue
@@ -25,15 +25,20 @@ fetchData($client).then((data) => {
   <div>
     <h1>Test BEL</h1>
     <p>fix bug</p>
-    <p class="red-text">{{ useProjects() }}</p>
+    <div class="card-wrapper">
+      <p class="red-text">{{ useProjects() }}</p>
+    </div>
     <img style="width: 300px"
       src="https://images.unsplash.com/photo-1698869955404-601e3b410184?auto=format&fit=crop&q=80&w=2656&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
       alt="image de test">
   </div>
 </template>
 
-<style scoped>
-.red-text {
-  color: red;
+<style scoped lang="scss">
+.card-wrapper{
+  background: lightblue;  
+  .red-text {
+    color: red;
+  }
 }
 </style>
