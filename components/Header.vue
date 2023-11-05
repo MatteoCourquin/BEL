700882c2323d558e10b20ea0fb6944ca79ca38b1
Augustin Briolon <augustin.briolon@fortuneo.com>
2023-11-05 12:46:35 +0100

    :lipstick: imrpove header style

--- components/Header.vue
+++ components/Header.vue
@@ -1,7 +1,7 @@
 <template>
-  <nav class="p-4 flex items-center justify-between">
-    <div class="flex items-center space-x-4">
-      <div>
+  <nav class="p-4 flex items-center justify-between max-w-default m-auto">
+    <div class="flex items-center space-x-4 relative">
+      <NuxtLink to="/" class="nav-link" @click="setActiveLink('/')">
         <!-- Logo SVG -->
         <svg
           width="97"
@@ -26,39 +26,77 @@
             stroke-width="2"
           />
         </svg>
+      </NuxtLink>
+      <div class="hidden md:flex space-x-6">
+        <NuxtLink
+          to="/projets"
+          class="nav-link uppercase"
+          @click="setActiveLink('/projets')"
+        >
+          Projets
+        </NuxtLink>
+        <NuxtLink
+          to="/presse"
+          class="nav-link uppercase"
+          @click="setActiveLink('/presse')"
+        >
+          Presse
+        </NuxtLink>
+        <NuxtLink
+          to="/equipe"
+          class="nav-link uppercase"
+          @click="setActiveLink('/equipe')"
+        >
+          Équipe
+        </NuxtLink>
       </div>
-      <div class="hidden md:flex space-x-4">
-        <NuxtLink to="/projets" class="active">Projets</NuxtLink>
-        <NuxtLink to="/presse" class="active">Presse</NuxtLink>
-        <NuxtLink to="/equipe" class="active">Équipe</NuxtLink>
-      </div>
+      <span
+        class="dot transition-dot"
+        :style="{
+          left: dotPosition + 'px',
+          display: dotDisplay,
+          marginLeft: '0',
+        }"
+      ></span>
     </div>
     <div class="md:hidden flex items-center space-x-4">
       <button @click="toggleMenu" class="text-white p-2">
         <svg
-          xmlns="http://www.w3.org/2000/svg"
           width="24"
           height="24"
           viewBox="0 0 24 24"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
         >
           <path
-            d="M12 3v2H5v2H12v2H3v2h9v2H5v2h7v2h-2v2H14v2H5v2h7v2H12v2h9v2h-7v2h7a2 2 0 01-2 2H5a2 2 0 01-2-2H0v-2h3v-2H0v-2h3v-2H0V7h3V5H0V3h3V1h2v2h7V1h2v2h7v2h-3v2h3v2h-3v2h3v2h-3v2h3v2h-3v2h3v2h-3v2h3v2h-3v2h3v2h-3v2h3v2h-3v2h3v2h-3a2 2 0 01-2-2z"
+            d="M4 6H20M4 12H20M4 18H20"
+            stroke="black"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
           />
         </svg>
       </button>
     </div>
     <div class="hidden md:flex">
-      <a href="/contact">Contact</a>
+      <NuxtLink
+        to="/contact"
+        class="nav-link bg-gold px-10 text-white py-1 rounded-md uppercase"
+        @click="setActiveLink('/contact')"
+      >
+        Contact
+      </NuxtLink>
     </div>
   </nav>
-  <div v-if="isMenuOpen" class="md:hidden">
-    <ul class="bg-blue-500 p-4 space-y-4">
-      <li><a href="/projets">Projets</a></li>
-      <li><a href="/presse">Presse</a></li>
-      <li><a href="/equipe">Équipe</a></li>
-      <li><a href="/contact">Contact</a></li>
-    </ul>
+  <div v-if="isMenuOpen" class="burgerMenu md:hidden w-screen h-screen fixed bg-white left-0 z-50">
+  <div class="flex flex-col items-center justify-center h-full">
+    <a href="/projets" class="text-black text-2xl mb-4 uppercase">Projets</a>
+    <a href="/presse" class="text-black text-2xl mb-4 uppercase">Presse</a>
+    <a href="/equipe" class="text-black text-2xl mb-4 uppercase">Équipe</a>
+    <a href="/contact" class="text-black text-2xl mb-4 uppercase">Contact</a>
   </div>
+</div>
+
 </template>
 
 <script>
@@ -67,21 +105,58 @@ export default {
   data() {
     return {
       isMenuOpen: false,
+      activeLink: "",
+      dotPosition: 0,
+      dotDisplay: "none",
     };
   },
   methods: {
     toggleMenu() {
       this.isMenuOpen = !this.isMenuOpen;
     },
+    setActiveLink(link) {
+      if (this.activeLink !== "/") {
+        const activeLinkClientRect = document
+        .querySelector(`.nav-link[href="${link}"]`)
+        .getBoundingClientRect();
+        
+        console.log(activeLinkClientRect);
+        const dotPosition =
+          activeLinkClientRect.right - activeLinkClientRect.width / 2 - 16;
+        this.dotPosition = dotPosition;
+        this.dotDisplay = "block";
+      } else {
+        this.dotDisplay = "none";
+      }
+    },
   },
   mounted() {
     window.addEventListener("resize", () => {
       if (window.innerWidth > 768) {
         this.isMenuOpen = false;
+        this.dotDisplay = "none";
       }
     });
   },
 };
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.dot {
+  width: 6px;
+  height: 6px;
+  background-color: #a7904a;
+  border-radius: 50%;
+  position: absolute;
+  bottom: 0px;
+  transform: translateX(-50%);
+}
+
+.transition-dot {
+  transition: left 0.3s;
+}
+
+.burgerMenu {
+  height: calc(100vh - 64px);
+}
+</style>
