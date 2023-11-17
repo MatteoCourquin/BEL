4b4413322edc670309293562c6a1f2b62da174aa
Augustin Briolon <augustin.briolon@fortuneo.com>
2023-11-17 23:43:46 +0100

    :sparkles: improve contact form

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,24 +1,47 @@
 <template>
   <section
     class="h-screen-header min-h-[800px] max-w-default py-y-default px-x-default mx-auto flex flex-col lg:flex-row justify-between items-center">
-    <div class="flex flex-col w-full lg:w-1/2 ">
+    <div v-if="!isFormSend" class="flex flex-col w-full lg:w-1/2 ">
       <h3>Demander un devis</h3>
       <form method='' ref='form' @submit.prevent='sendEmail'>
         <div class="w-full flex flex-wrap sm:flex-nowrap gap-4 mt-4">
-          <input type='text' placeholder='Jean' class="w-full sm:w-1/2 p-2 rounded-md border-dark-grey border" />
-          <input type='text' placeholder='Dupont' class="w-full sm:w-1/2  p-2 rounded-md border-dark-grey border" />
+          <div class="w-full flex flex-col items-start">
+            <label for='firstname' class="w-full">Prénom</label>
+            <input type='text' placeholder='Jean' class="w-full p-2 rounded-md border-dark-grey border" ref="firstname"
+              name="firstname" v-model="formData.firstname" @input="validateForm" />
+          </div>
+          <div class="w-full flex flex-col items-start">
+            <label for='lastname' class="w-full">Nom</label>
+            <input type='text' placeholder='Dupont' class="w-full  p-2 rounded-md border-dark-grey border" ref="lastname"
+              name="lastname" v-model="formData.lastname" @input="validateForm" />
+          </div>
         </div>
         <div class="w-full flex flex-wrap sm:flex-nowrap gap-4 mt-4">
-          <input type='text' placeholder='jean.dupont@gmail.com' class="w-full sm:w-1/2 p-2 rounded-md border-dark-grey border" />
-          <input type='text' placeholder='0612121212' class="w-full sm:w-1/2 p-2 rounded-md border-dark-grey border" />
+          <div class="w-1/2 flex flex-col items-start">
+            <label for='mail' class="w-full">Mail</label>
+            <input type='mail' placeholder='jean.dupont@gmail.com' class="w-full p-2 rounded-md border-dark-grey border"
+              ref="mail" name="mail" v-model="formData.mail" @input="validateForm" />
+          </div>
+          <div class="w-1/2 flex flex-col items-start">
+            <label for='tel' class="w-full">Téléphone</label>
+            <input type='tel' placeholder='0612121212' class="w-full p-2 rounded-md border-dark-grey border" ref="tel"
+              name="tel" v-model="formData.tel" @input="validateForm" />
+          </div>
         </div>
-        <textarea placeholder='Je vous contact pour ...'
-          class="mt-4 w-full p-2 rounded-md border-dark-grey border"></textarea>
-        <button type='submit' class="bg-gold mt-6 px-10 py-1 !text-white rounded-md">Envoyer</button>
+        <div class="w-full mt-4 flex flex-col items-start">
+          <label for='description' class="w-full">Description</label>
+          <textarea placeholder='Je vous contact pour ...' class="w-full p-2 rounded-md border-dark-grey border"
+            ref="description" name="description" v-model="formData.description" @input="validateForm"></textarea>
+        </div>
+        <button type='submit' :class="{ 'bg-gold': isFormValid, 'bg-dark-gray': !isFormValid }" :disabled="!isFormValid"
+          class="mt-6 px-10 py-1 !text-white rounded-md">Envoyer</button>
       </form>
     </div>
+    <div v-else class="flex flex-col w-full lg:w-1/2 ">
+      <h3>Merci, nous vous recontacterons très vite !</h3>
+    </div>
     <div class="flex justify-end w-full lg:w-1/2">
-      <div class="border-image m-4 w-1/2 lg:w-1/2 min-w-[250px] min-h-[16vh] sm:h-[30vh]  -translate-x-4">
+      <div class="border-image m-4 w-full lg:w-1/2 min-w-[250px] h-48 min-h-[16vh] sm:h-[30vh]  -translate-x-4">
         <img src="/images/architect.jpg" alt="illustration d'architecture"
           class="rounded-small object-cover w-full h-full" />
       </div>
@@ -32,17 +55,35 @@ import emailjs from '@emailjs/browser';
 export default {
   name: 'Contact',
   data() {
-    return {};
+    return {
+      isFormValid: false,
+      isFormSend: false,
+      formData: {
+        firstname: '',
+        lastname: '',
+        mail: '',
+        tel: '',
+        description: '',
+      },
+    };
   },
   methods: {
+    validateForm() {
+      this.isFormValid = Object.values(this.formData).every(value => value !== '');
+    },
     sendEmail() {
-      emailjs.sendForm(import.meta.env.VITE_EMAILJS_SERVICE_ID, import.meta.env.VITE_EMAILJS_TEMPLATE_ID, this.$refs.form, import.meta.env.VITE_EMAILJS_PUBLIC_KEY)
+      emailjs.send(import.meta.env.VITE_EMAILJS_SERVICE_ID, import.meta.env.VITE_EMAILJS_TEMPLATE_ID, this.formData, import.meta.env.VITE_EMAILJS_PUBLIC_KEY)
         .then((result) => {
-            console.log('SUCCESS!', result.text);
+          console.log('SUCCESS!', result.status, result.text);
+          this.isFormSend = true;
+          setTimeout(() => {
+            this.$router.push('/');
+          }, 2000);
         }, (error) => {
-            console.log('FAILED...', error.text);
+          console.log('FAILED GROS NUL', error);
+          alert('Une erreur est survenue, veuillez réessayer');
         });
-    }
+    },
   }
 }
 </script>
