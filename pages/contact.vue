8aea086518a81a8b30377f3f5b0f138ffd3d262e
Augustin Briolon <augustin.briolon@fortuneo.com>
2023-11-23 17:30:03 +0100

    :lipstick: improve input style

--- pages/contact.vue
+++ pages/contact.vue
@@ -7,30 +7,30 @@
         <div class="w-full flex flex-wrap sm:flex-nowrap gap-4 mt-4">
           <div class="w-full flex flex-col items-start">
             <label for='firstname' class="w-full">Prénom</label>
-            <input type='text' placeholder='Jean' class="w-full p-2 rounded-md border-dark-grey border" ref="firstname"
+            <input type='text' placeholder='Jean'  class="w-full p-2 rounded-md bg-white border-dark-grey border focus-border-gold invalid:border-red-500" ref="firstname"
               name="firstname" v-model="formData.firstname" @input="validateForm" />
           </div>
           <div class="w-full flex flex-col items-start">
             <label for='lastname' class="w-full">Nom</label>
-            <input type='text' placeholder='Dupont' class="w-full  p-2 rounded-md border-dark-grey border" ref="lastname"
+            <input type='text' placeholder='Dupont' class="w-full  p-2 rounded-md bg-white border-dark-grey border focus-border-gold invalid:border-red-500" ref="lastname"
               name="lastname" v-model="formData.lastname" @input="validateForm" />
           </div>
         </div>
         <div class="w-full flex flex-wrap sm:flex-nowrap gap-4 mt-4">
           <div class="w-1/2 flex flex-col items-start">
             <label for='mail' class="w-full">Mail</label>
-            <input type='mail' placeholder='jean.dupont@gmail.com' class="w-full p-2 rounded-md border-dark-grey border"
+            <input type='email' placeholder='jean.dupont@gmail.com' class="w-full p-2 rounded-md bg-white border-dark-grey border focus-border-gold invalid:border-red-500"
               ref="mail" name="mail" v-model="formData.mail" @input="validateForm" />
           </div>
           <div class="w-1/2 flex flex-col items-start">
             <label for='tel' class="w-full">Téléphone</label>
-            <input type='tel' placeholder='0612121212' class="w-full p-2 rounded-md border-dark-grey border" ref="tel"
+            <input type='tel' placeholder='0612121212' class="w-full p-2 rounded-md bg-white border-dark-grey border focus-border-gold invalid:border-red-500" ref="tel"
               name="tel" v-model="formData.tel" @input="validateForm" />
           </div>
         </div>
         <div class="w-full mt-4 flex flex-col items-start">
           <label for='description' class="w-full">Description</label>
-          <textarea placeholder='Je vous contact pour ...' class="w-full p-2 rounded-md border-dark-grey border"
+          <textarea placeholder='Je vous contact pour ...' class="w-full p-2 rounded-md bg-white border-dark-grey border focus-border-gold invalid:border-red-500"
             ref="description" name="description" v-model="formData.description" @input="validateForm"></textarea>
         </div>
         <button type='submit' :class="{ 'bg-gold': isFormValid, 'bg-dark-gray': !isFormValid }" :disabled="!isFormValid"
@@ -69,6 +69,7 @@ export default {
   },
   methods: {
     validateForm() {
+      // verifie que formData.tel est bien un numero de tel avec une regex
       this.isFormValid = Object.values(this.formData).every(value => value !== '');
     },
     sendEmail() {
@@ -90,6 +91,9 @@ export default {
 
 <style scoped lang='scss'>
 @import "@/scss/main.scss";
+.focus-border-gold:focus {
+  border-color: gold;
+}
 
 .border-image {
 
