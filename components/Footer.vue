09d879e9df64e93960459f0f0c9bc3ce0bf5829b
Augustin Briolon <augustin.briolon@fortuneo.com>
2023-12-10 16:58:46 +0100

    :lipstick: redesign footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,67 +1,68 @@
 <template>
   <footer class="bg-black py-16">
     <div class="max-w-default h-fit mx-auto px-x-default">
-      <div class="w-full flex flex-col md:flex-row justify-between items-start md:items-center my-6">
-        <NuxtLink to="/" aria-label="Logo de l'entreprise BEL">
-          <svg width="97" height="40" viewBox="0 0 97 40" fill="none" xmlns="http://www.w3.org/2000/svg"
-            class="stroke-white hover:stroke-gold transition-colors">
-            <path d="M7.84616 20.7692V21.7692H8.84616H29.3846V39H1V1H29.3846V17.0769H8.84616H7.84616V18.0769V20.7692Z"
-              stroke-width="2" />
-            <path
-              d="M40.9228 23.8462V24.8462H41.9228H62.0767V39H34.0767V1H62.0767V13.6154H41.9228H40.9228V14.6154V18.0769V19.0769H41.9228H47.8459V19.7692H41.9228H40.9228V20.7692V23.8462Z"
-              stroke-width="2" />
-            <path d="M72.3442 20.7692V21.7692H73.3442H95.5384V39H67.1538V1H72.3442V20.7692Z" stroke-width="2" />
-            <line x1="11" y1="9" x2="19" y2="9" stroke-width="2" />
-            <line x1="11" y1="30" x2="19" y2="30" stroke-width="2" />
-          </svg>
-        </NuxtLink>
-        <p class="text-white text-xl  mt-6 md:mt-0">Demande de renseignements</p>
-      </div>
-      <div class="w-full flex flex-col-reverse md:flex-row justify-between items-start md:items-center my-6">
-        <div class="flex flex-wrap  mt-3 md:mt-0">
-          <p class="text-white flex items-center mr-8">
-            <span class="mr-2">
-              <svg width="16" height="12" viewBox="0 0 16 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path
-                  d="M0.00292969 1.884L7.99993 5.882L15.9969 1.884C15.9673 1.37444 15.744 0.895488 15.3728 0.545227C15.0015 0.194965 14.5103 -9.35847e-05 13.9999 3.36834e-08H1.99993C1.48951 -9.35847e-05 0.998377 0.194965 0.627107 0.545227C0.255838 0.895488 0.0325338 1.37444 0.00292969 1.884Z"
-                  fill="white" />
-                <path
-                  d="M16 4.11816L8 8.11816L0 4.11816V10.0002C0 10.5306 0.210714 11.0393 0.585786 11.4144C0.960859 11.7895 1.46957 12.0002 2 12.0002H14C14.5304 12.0002 15.0391 11.7895 15.4142 11.4144C15.7893 11.0393 16 10.5306 16 10.0002V4.11816Z"
-                  fill="white" />
-              </svg>
-            </span>
-            <a href="mailto:etudes@be-legavre.fr">etudes@be-legavre.com</a>
-          </p>
-          <p class="text-white flex items-center mr-8">
-            <span class="mr-2">
-              <svg width="10" height="12" viewBox="0 0 10 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path fill-rule="evenodd" clip-rule="evenodd"
-                  d="M1.41949 1.45584C2.32831 0.523683 3.56094 0 4.8462 0C6.13147 0 7.3641 0.523683 8.27292 1.45584C9.18174 2.38801 9.69231 3.65229 9.69231 4.97056C9.69231 6.28884 9.18174 7.55312 8.27292 8.48528L4.8462 12L1.41949 8.48528C0.969461 8.02374 0.612474 7.47579 0.368916 6.87274C0.125358 6.26968 0 5.62332 0 4.97056C0 4.31781 0.125358 3.67145 0.368916 3.06839C0.612474 2.46533 0.969461 1.91739 1.41949 1.45584ZM4.8462 6.39065C5.21341 6.39065 5.56557 6.24103 5.82522 5.97472C6.08487 5.7084 6.23074 5.34719 6.23074 4.97056C6.23074 4.59393 6.08487 4.23273 5.82522 3.96641C5.56557 3.70009 5.21341 3.55047 4.8462 3.55047C4.479 3.55047 4.12684 3.70009 3.86719 3.96641C3.60754 4.23273 3.46167 4.59393 3.46167 4.97056C3.46167 5.34719 3.60754 5.7084 3.86719 5.97472C4.12684 6.24103 4.479 6.39065 4.8462 6.39065Z"
-                  fill="white" />
-              </svg>
-            </span>
-            7 rue Surcouf – ZA D’Atlantheix - 56450 THEIX-NOYALO
-          </p>
-          <p class="text-white flex items-center mr-8">
-            <span class="mr-2">
-              <svg width="12" height="13" viewBox="0 0 12 13" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path
-                  d="M0 1.21094C0 1.01203 0.0790176 0.82126 0.21967 0.680607C0.360322 0.539955 0.551088 0.460938 0.75 0.460938H2.36475C2.54228 0.461019 2.71402 0.524073 2.84943 0.638882C2.98484 0.753691 3.07514 0.912812 3.10425 1.08794L3.65925 4.41419C3.68577 4.5726 3.66073 4.73534 3.58782 4.87846C3.51491 5.02158 3.39799 5.13751 3.25425 5.20919L2.09325 5.78894C2.50958 6.82068 3.12963 7.7579 3.91633 8.54461C4.70304 9.33131 5.64026 9.95136 6.672 10.3677L7.2525 9.20669C7.32414 9.06309 7.43995 8.94626 7.58291 8.87336C7.72588 8.80046 7.88845 8.77534 8.04675 8.80169L11.373 9.35669C11.5481 9.3858 11.7072 9.4761 11.8221 9.6115C11.9369 9.74691 11.9999 9.91866 12 10.0962V11.7109C12 11.9099 11.921 12.1006 11.7803 12.2413C11.6397 12.3819 11.4489 12.4609 11.25 12.4609H9.75C4.365 12.4609 0 8.09594 0 2.71094V1.21094Z"
-                  fill="white" />
-              </svg>
-            </span>
-            <a href="tel:0606060606">02 97 47 53 27</a>
-          </p>
+      <div class="flex flex-col md:flex-row justify-between space-y-8 md:space-y-0">
+        <div class="flex flex-col justify-between space-y-8">
+          <NuxtLink to="/contact"
+            class="w-fit bg-white hover:bg-gold !text-black hover:!text-white transition-colors px-10 py-1 rounded-small uppercase font-inter-semi-bold">
+            Contact
+          </NuxtLink>
+          <div class="w-full flex flex-col sm:flex-row items-start space-y-8 sm:space-y-0">
+            <div class="flex flex-col space-y-4">
+              <p class="text-white flex items-center mr-8">
+                Bureau d'Études Legavre :
+              </p>
+              <p class="text-white flex items-center mr-8">
+                7 rue Surcouf – ZA D’Atlantheix <br> 56450 THEIX-NOYALO
+              </p>
+            </div>
+            <div class="flex flex-col space-y-4">
+              <p class="text-white flex items-center mr-8">
+                <span class="mr-2">
+                  <svg width="16" height="12" viewBox="0 0 16 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path
+                      d="M0.00292969 1.884L7.99993 5.882L15.9969 1.884C15.9673 1.37444 15.744 0.895488 15.3728 0.545227C15.0015 0.194965 14.5103 -9.35847e-05 13.9999 3.36834e-08H1.99993C1.48951 -9.35847e-05 0.998377 0.194965 0.627107 0.545227C0.255838 0.895488 0.0325338 1.37444 0.00292969 1.884Z"
+                      fill="white" />
+                    <path
+                      d="M16 4.11816L8 8.11816L0 4.11816V10.0002C0 10.5306 0.210714 11.0393 0.585786 11.4144C0.960859 11.7895 1.46957 12.0002 2 12.0002H14C14.5304 12.0002 15.0391 11.7895 15.4142 11.4144C15.7893 11.0393 16 10.5306 16 10.0002V4.11816Z"
+                      fill="white" />
+                  </svg>
+                </span>
+                <a href="mailto:etudes@be-legavre.fr">etudes@be-legavre.com</a>
+              </p>
+              <p class="text-white flex items-center mr-8">
+                <span class="mr-2">
+                  <svg width="12" height="13" viewBox="0 0 12 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path
+                      d="M0 1.21094C0 1.01203 0.0790176 0.82126 0.21967 0.680607C0.360322 0.539955 0.551088 0.460938 0.75 0.460938H2.36475C2.54228 0.461019 2.71402 0.524073 2.84943 0.638882C2.98484 0.753691 3.07514 0.912812 3.10425 1.08794L3.65925 4.41419C3.68577 4.5726 3.66073 4.73534 3.58782 4.87846C3.51491 5.02158 3.39799 5.13751 3.25425 5.20919L2.09325 5.78894C2.50958 6.82068 3.12963 7.7579 3.91633 8.54461C4.70304 9.33131 5.64026 9.95136 6.672 10.3677L7.2525 9.20669C7.32414 9.06309 7.43995 8.94626 7.58291 8.87336C7.72588 8.80046 7.88845 8.77534 8.04675 8.80169L11.373 9.35669C11.5481 9.3858 11.7072 9.4761 11.8221 9.6115C11.9369 9.74691 11.9999 9.91866 12 10.0962V11.7109C12 11.9099 11.921 12.1006 11.7803 12.2413C11.6397 12.3819 11.4489 12.4609 11.25 12.4609H9.75C4.365 12.4609 0 8.09594 0 2.71094V1.21094Z"
+                      fill="white" />
+                  </svg>
+                </span>
+                <a href="tel:0606060606">02 97 47 53 27</a>
+              </p>
+            </div>
+          </div>
         </div>
-        <NuxtLink to="/contact"
-          class="bg-white hover:bg-gold !text-black hover:!text-white transition-colors px-10 py-1 rounded-small uppercase font-inter-semi-bold">
-          Contact
-        </NuxtLink>
+        <div class=" flex justify-start md:justify-end items-end">
+          <NuxtLink to="/" aria-label="Logo de l'entreprise BEL">
+            <svg width="97" height="40" viewBox="0 0 97 40" fill="none" xmlns="http://www.w3.org/2000/svg"
+              class="stroke-white hover:stroke-gold transition-colors">
+              <path d="M7.84616 20.7692V21.7692H8.84616H29.3846V39H1V1H29.3846V17.0769H8.84616H7.84616V18.0769V20.7692Z"
+                stroke-width="2" />
+              <path
+                d="M40.9228 23.8462V24.8462H41.9228H62.0767V39H34.0767V1H62.0767V13.6154H41.9228H40.9228V14.6154V18.0769V19.0769H41.9228H47.8459V19.7692H41.9228H40.9228V20.7692V23.8462Z"
+                stroke-width="2" />
+              <path d="M72.3442 20.7692V21.7692H73.3442H95.5384V39H67.1538V1H72.3442V20.7692Z" stroke-width="2" />
+              <line x1="11" y1="9" x2="19" y2="9" stroke-width="2" />
+              <line x1="11" y1="30" x2="19" y2="30" stroke-width="2" />
+            </svg>
+          </NuxtLink>
+        </div>
       </div>
+
       <hr class="text-white my-16">
-      <div class="flex flex-col-reverse md:flex-row items-start md:items-center justify-between text-white">
-        <p class="text-sm text-white mt-3 md:mt-0">© {{ dateYear }} Bureau Études Legavre. Tous droits réservés.</p>
-        <p class="text-sm text-white mb-3 md:mb-0">Cookie</p>
+      <div class="flex items-center justify-center text-white">
+        <p class="text-sm text-white mt-3 md:mt-0">© 1988 - {{ dateYear }} • Zèle Solutions. Tous droits réservés.</p>
       </div>
     </div>
   </footer>
